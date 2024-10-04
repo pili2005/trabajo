9cefbc37b049eb6b3503f83ea7be5ff6366d1ce1
pili2005 <pilargallardo0016@gmail.com>
2024-10-04 11:10:25 -0300

    agregue comprimir con f aux contador

--- tp1-factorizacion-primos-pili2005-main/src/TP1.hs
+++ tp1-factorizacion-primos-pili2005-main/src/TP1.hs
@@ -6,6 +6,7 @@
 -- Comision : 2
 
 module TP1 (factorizar, divide, divisores, esPrimo, divisoresPrimos, comprimir) where
+import GHC.Stack.CCS (whereFrom)
 
 -- | Dados dos enteros X e Y, retorna True si X divide a Y, caso contrario False
 divide :: Int -> Int -> Bool
@@ -14,8 +15,7 @@ divide x y = if x `mod` y == 0 then True else False
 -- | Dado un número X, retorna la lista de todos los divisores de X.
 -- Ej: divisores 10 = [1,2,5,10]
 divisores :: Int -> [Int]
-divisores 1 = [1]
-divisores x = [n | n <- [2..x], x `mod` n == 0]
+divisores x = [n | n <- [1..x], x `mod` n == 0]
 
 -- | Dado un número, determina si es un número primo.
 esPrimo :: Int -> Bool
@@ -30,7 +30,15 @@ divisoresPrimos x = [p | p <- divisores x , esPrimo p]
 -- | Dada una lista de números, comprime la lista contando las repeticiones adyacentes.
 -- Ej: comprimir [2,2,2,5,2,2] = [(2,3), (5,1), (2,2)]
 comprimir :: [Int] -> [(Int, Int)]
-comprimir = undefined
+comprimir [] = []
+comprimir (x:xs) = contador x 1 xs
+
+contador :: Int -> Int -> [Int] -> [(Int,Int)]
+contador num cont [] = [(num,cont)]
+contador num cont (y:ys)
+        |num == y = contador num (cont + 1) ys
+        |otherwise = (num, cont) : contador y 1 ys
+
 
 -- | Función principal. 
 -- Implementar usando las funciones previas, funciones definidas en Haskell y toda otra función auxiliar que sea necesaria.
